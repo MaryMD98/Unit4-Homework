0be8643926b2391e7aee9787d37328cab170587f
Maribel Montes <maribel.doris@gmail.com>
2022-01-06 16:49:51 -0600

    added the header and main to html and the wuestions and answers to be displayed

---
+++ Assets/js/script.js
@@ -0,0 +1,36 @@
+// coding quiz challenge
+
+// commonly used data types Do Not include:
+// 1). strings
+// 2. booleans //wrong
+// 3. alerts
+// 4. numbers
+
+// the condition in an if/else statement is enclosed within ___.
+
+// 1.quotes
+// 2.curly brackets
+// 3.parenthesis //correct
+// 4.square brackets
+
+// arrays in JavaScript can be used to Store  ___
+
+// 1.numbers and strings
+// 2.other arrays
+// 3.booleans 
+// 4.all of the above //correct
+
+// string values must be enclosed within _____ when being assigned to varibles
+// 1.commas
+// 2.curly brackets // wrong
+// 3.quotes
+// 4.parenthesis
+
+// A very useful tool used during development and debugging for printing content to the debugger is 
+// 1. JavaScript
+// 2.terminal/bash 
+// 3.for loops //wrong
+// 4.console.log 
+
+
+
