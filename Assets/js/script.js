a3b7e1ec47286a05060ef7014c760d26a0715172
Maribel Montes <maribel.doris@gmail.com>
2022-01-07 08:45:32 -0600

    added startgame and gameover function to hide and show before and after quiz

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,12 +7,15 @@ var multipleAnswer1 = document.querySelector("#multianswer1");
 var multipleAnswer2 = document.querySelector("#multianswer2");
 var multipleAnswer3 = document.querySelector("#multianswer3");
 var multipleAnswer4 = document.querySelector("#multianswer4");
+var questionTitle = document.querySelector(".container");
+var gameOverScreen = document.querySelection(".gameover");
 
 var questionNumCount = 0;
 var answerIndex = 0;
 var userAnswerCnt = 0;
 var correctCount = 0;
 var wrongCount = 0;
+var timerCount = 120;
 
 // Array  of string questions
 var questionString = [
@@ -50,7 +53,7 @@ function questionDisplay(){
         answersH.children[x].textContent = answerString[answerIndex];
         answerIndex++;
     }
-    
+
 }
 
 //check if answer selected is correct
@@ -73,10 +76,61 @@ function checkifCorrect(answer){
     questionNumCount++;
 }
 
+//start of game, timer starts, first question is displied, header and text are hidden
+function startGame(){
+    $("#start").hide();
+    $("#quizTitle").hide();
+    $("#quizIns").hide();
+    questionTitle.children[0].textContent = "Question"
+
+    startTimer();
+    // $("h1").hide();
+    // $("p").hide();
+}
+
+// $(document).ready(function(){
+//     $("#start").click(function(){
+//         $("h1").hide();
+//         $("p").hide();
+//     });
+// });
+
+//Game over check correct and wrong answers, display save initial and score
+function gameOver(){
+    // displays All done! your total score is ..  enter initials and submit button
+   $(".container").hide();
+
+
+//    document.body.appendChild()
+
+}
+
+ // after submit display highscores with  with go back and clear highscore button 
+function showResults(){
+
+}
+
 // timer function will  decrement 
+function startTimer(){
+
+    timer = setInterval(function(){
+        timerCount--;
+
+    // while timer is on quiz is on, 
+    // once timer is done, check the wrong and correct answers
+    // game is done 
+        if(timerCount === 0){
+            gameOver();
+
+        }
+    }, 1000);
+
+}
+
 // start button -> on click display question
 startBTN.addEventListener("click", function(event){
     event.preventDefault();
+    startGame();
     questionDisplay();
 });
 
@@ -88,7 +142,10 @@ multipleAnswer1.addEventListener("click", function(event){
 
     if (questionNumCount < 5){
         questionDisplay();
-    }    
+    }  else {
+        gameOver();
+    }
+
 });
 multipleAnswer2.addEventListener("click", function(event){
     event.preventDefault();
@@ -96,7 +153,9 @@ multipleAnswer2.addEventListener("click", function(event){
 
     if (questionNumCount < 5){
         questionDisplay();
-    }    
+    } else {
+        gameOver();
+    }
 });
 multipleAnswer3.addEventListener("click", function(event){
     event.preventDefault();
@@ -104,7 +163,9 @@ multipleAnswer3.addEventListener("click", function(event){
 
     if (questionNumCount < 5){
         questionDisplay();
-    }    
+    } else {
+        gameOver();
+    } 
 });
 multipleAnswer4.addEventListener("click", function(event){
     event.preventDefault();
@@ -112,7 +173,9 @@ multipleAnswer4.addEventListener("click", function(event){
 
     if (questionNumCount < 5){
         questionDisplay();
-    }    
+    }  else {
+        gameOver();
+    } 
 });
 
 // questionDisplay();
